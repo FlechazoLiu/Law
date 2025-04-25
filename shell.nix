c1df48d859b44b211c4a34c0f62af376e404a343
timechess <tyc221@ruc.edu.cn>
2025-04-25 22:23:17 +0800

    blueprint

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}:
+pkgs.mkShell rec {
+  name = "python-venv";
+  venvDir = "./.venv";
+
+  packages = with pkgs; [texliveFull graphviz pdf2svg elan gnumake poetry triton-llvm];
+
+  LD_LIBRARY_PATH =
+    pkgs.lib.makeLibraryPath ([pkgs.stdenv.cc.cc] ++ packages);
+
+  shellHook = ''
+    poetry install
+  '';
+}
